bbc4e5f21ba9bf6b72c5669e66e0072f206a0618
Shravan Kumar Nunsavath <93977456+Nshravankumar4@users.noreply.github.com>
2026-01-25 23:05:43 +0530

    Delete build directory

--- build/CMakeFiles/math_utils.dir/src/math_utils.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/math_utils.dir/src/math_utils.cpp.o: \
- /mnt/d/Linux/Linux-Application_CXX/src/math_utils.cpp \
- /usr/include/stdc-predef.h \
- /mnt/d/Linux/Linux-Application_CXX/include/math_utils.h
--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/d/Linux/Linux-Application_CXX
-# Build directory: /mnt/d/Linux/Linux-Application_CXX/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/mnt/d/Linux/Linux-Application_CXX/build/runTests[1]_include.cmake")
